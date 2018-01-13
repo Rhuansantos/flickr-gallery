2f191767db5e4ec693e12919993568f22c89d364
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-13 15:12:48 -0500

    update stylesheet and include clock

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 import Gallery from './gallery';
-import Util from 'util';
+import Util from './util';
 import TemplateGallery from './gallery_template';
+
+
+
+
 
 // waiting for the page loading
 window.addEventListener('load',() => {
 
-    const data = new Date();
-    console.log(data);
+    Util.startTime();
+    
     // API KEY
     const apiKey = "a31291fbb92c2078dc081e40fa6ab76c";
     const searchForm = document.querySelector('.searchForm')
--- src/js/util.js
+++ src/js/util.js
@@ -6,4 +6,22 @@ export default class Util {
     static loaded() {
         console.log('loaded');
     }
+    static startTime() {
+
+        const checkTime = (i) => {
+            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+            return i;
+        }
+
+        let today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let ampm = h >= 12 ? 'pm' : 'am';
+        // let s = today.getSeconds();
+        m = checkTime(m);
+        // s = checkTime(s);
+        document.querySelector('.time').innerHTML =
+        h + ":" + m + " " + ampm;
+        const t = setTimeout(Util.startTime, 1000);
+    }
 }
